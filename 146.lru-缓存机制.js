3852afadec8e759128342f53d8424ef0ede42824
hubingkang <squarewheel@163.com>
2022-03-01 18:18:43 +0800

    feat(数据结构): LRU

--- 146.lru-缓存机制.js
+++ 146.lru-缓存机制.js
@@ -13,6 +13,7 @@ var LRUCache = function(capacity) {
   this.capacity = capacity;
   this.hashMap = new Map();
 
+  // 建立 头尾的空节点
   this.head = new DLinkNodes();
   this.tail = new DLinkNodes();
   this.head.next = this.tail;
@@ -25,7 +26,6 @@ var LRUCache = function(capacity) {
  */
 LRUCache.prototype.get = function(key) {
   if (!this.hashMap.has(key)) return -1;
-
   const node = this.hashMap.get(key);
   this.moveToHead(node);
   return node.value;
@@ -38,44 +38,38 @@ LRUCache.prototype.get = function(key) {
  */
 LRUCache.prototype.put = function(key, value) {
   const node = this.hashMap.get(key);
-
-  if (!node) {
-    const newNodes = new DLinkNodes(key, value);
-    this.addToHead(newNodes);
-
-    if (this.size > this.capacity) {
-      // 删除最后一个元素
-      this.remove(this.tail.prev);
-    }
-  } else {
-    // 更新值
+  
+  // 如果存在该节点
+  if (node) {
     node.value = value;
     this.moveToHead(node);
+  } else {
+    // 如果大于容量
+    if (this.size >= this.capacity) {
+      const tailNode = this.tail.prev;
+      this.hashMap.delete(tailNode.key);
+      this.tail.prev = tailNode.prev;
+      tailNode.prev.next = this.tail;
+      this.size--;
+    }
+
+    const newNode = new DLinkNodes(key, value);
+    this.head.next.prev = newNode;
+    newNode.next = this.head.next;
+    newNode.prev = this.head;
+    this.head.next = newNode;
+    this.hashMap.set(key, newNode);
+    this.size++;
   }
 };
 
-
-LRUCache.prototype.addToHead = function(node) {
+LRUCache.prototype.moveToHead = function(node) {
+  node.next.prev = node.prev;
+  node.prev.next = node.next;
   node.prev = this.head;
   node.next = this.head.next;
   this.head.next.prev = node;
   this.head.next = node;
-
-  this.hashMap.set(node.key, node);
-  this.size++;
-}
-
-LRUCache.prototype.remove = function(node) {
-  node.next.prev = node.prev;
-  node.prev.next = node.next;
-
-  this.hashMap.delete(node.key);
-  this.size--;
-}
-
-LRUCache.prototype.moveToHead = function(node) {
-  this.remove(node);
-  this.addToHead(node);
 }
 
 var DLinkNodes = function(key, value) {
